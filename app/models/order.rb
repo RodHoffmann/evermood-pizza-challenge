e7d9d6b28ac40b8e8b0e5d1cc39eb58318113df1
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2024-11-17 19:25:53 +0100

    Added Business Logic to order and order_item models and finished the order index view. Still needs refactoring and submit the form without reloading with turbo

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,6 +9,36 @@ class Order < ApplicationRecord
   has_many :size_multipliers, through: :order_items
 
   def self.open_orders
-    open_orders = Order.where(state: 'OPEN')
+    Order.where(state: 'OPEN')
+  end
+
+  def total_price
+    price_before_discount_and_promotions = order_items.sum(&:total_item_price)
+    price_before_discount = apply_promotion_codes(price_before_discount_and_promotions)
+    discount_code ? apply_discount_code(price_before_discount).round(2) : price_before_discount.round(2)
+  end
+
+  private
+
+  def apply_promotion_codes(price_before_discount_and_promotions)
+    promotion_codes&.each do |promotion_code|
+      target_pizza = promotion_code.target
+      target_size = promotion_code.target_size
+      from = promotion_code.from
+      to = promotion_code.to
+      target_pizza_price = Pizza.where(name: target_pizza).first.price * SizeMultiplier.where(size: target_size).first.multiplier
+      number_target_pizzas = order_items
+                             .joins(:pizza, :size_multiplier)
+                             .where(pizzas: { name: target_pizza }, size_multipliers: { size: target_size })
+                             .count
+      if number_target_pizzas.positive? && number_target_pizzas >= from
+        price_before_discount_and_promotions -= ((number_target_pizzas / from) * to) * target_pizza_price
+      end
+    end
+    price_before_discount_and_promotions
+  end
+
+  def apply_discount_code(price_before_discount)
+    price_before_discount * (1 - discount_code.deduction_in_percent / 100)
   end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,12 @@ class OrderItem < ApplicationRecord
   belongs_to :size_multiplier
   belongs_to :order
   has_many :order_item_ingredient_modifications, dependent: :destroy
+
+  def total_item_price
+    price_for_extra_ingredients = order_item_ingredient_modifications
+                                  .where(modification_type: 'add')
+                                  .joins(:ingredient)
+                                  .sum('ingredients.price') || 0
+    (pizza.price + price_for_extra_ingredients) * size_multiplier.multiplier
+  end
 end
