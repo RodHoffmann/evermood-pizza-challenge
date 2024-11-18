a9ea4dfbb28308d3af81301937c1e2f26e4ff714
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2024-11-18 12:04:06 +0100

    Set RSpec and Factory Bot basic config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,11 +37,24 @@ RSpec.configure do |config|
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
+  # FactoryBot methods allowed directly in tests
+  config.include FactoryBot::Syntax::Methods
 
+  # database_cleaner for managing test database
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
